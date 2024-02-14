216f2a9ec5b30af5f3b50feeceacfce3df5b955e
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-02-14 11:30:58 +0100

    All methods class of horse has been tested

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,13 @@
 public class Test {
     public static void main(String[] args) {
-        var c = '\u001D';
-        System.out.println(c);
+        /*var c = '\u001D';
+        System.out.println(c);*/
+        for (int i = 0; i < 5; i++) {
+            System.out.println(getRandomDouble(0.2, 0.9));
+        }
+    }
+
+    public static double getRandomDouble(double min, double max) {
+        return (Math.random() * (max - min)) + min;
     }
 }
--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,8 +1,15 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class HorseTest {
 
    /* @BeforeEach
@@ -10,12 +17,12 @@ class HorseTest {
     }*/
 
     @Test
-    void ifFirstParamInConstructorNullThrowIllegalArgException() {
+    void constructorNameThrowIAE() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Horse(null, 2, 2));
     }
 
     @Test
-    void ifFirstParamInConstructorNullGetMessageFromException() {
+    void constructorNameGetMessageFromIAE() {
         String message = "";
         try {
             new Horse(null, 2, 2);
@@ -26,14 +33,14 @@ class HorseTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"\t", "", " ", "\r", "\n",})
-    void ifFirstParamInConstructorWhitespaceOrNotCharsThrowIllegalArgException(String name) {
+    @ValueSource(strings = {"\t", "", " ", "\r", "\n", "\f"})
+    void constructorNameWhitespaceOrNotCharsThrowIAE(String name) {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Horse(name, 2, 2));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"\t", "", " ", "\r", "\n",})
-    void ifFirstParamInConstructorWhitespaceOrNotCharsGetMessageFromIllegalArgException(String name) {
+    void constructorNameWhitespaceOrNotCharsGetMessageFromIAE(String name) {
         String message = "";
         try {
             new Horse(name, 2, 2);
@@ -44,12 +51,12 @@ class HorseTest {
     }
 
     @Test
-    void ifSecondParamInConstructorNegativeNumberThrowIllegalArgException() {
+    void constructorSpeedNegativeNumberThrowIAE() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Horse("horse", -1, 2));
     }
 
     @Test
-    void ifSecondParamInConstructorNegativeNumberGetMessageFromException() {
+    void constructorSpeedNegativeNumberGetMessageFromIAE() {
         String message = "";
         try {
             new Horse("horse", -2, 2);
@@ -57,5 +64,54 @@ class HorseTest {
             message = ex.getMessage();
         }
         Assertions.assertEquals("Speed cannot be negative.", message);
+    }
+
+    @Test
+    void constructorDistanceNegativeNumberThrowIAE() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Horse("horse", 1, -2));
+    }
+
+    @Test
+    void constructorDistanceNegativeNumberGetMessageFromIAE() {
+        String message = "";
+        try {
+            new Horse("horse", 2, -2);
+        } catch (IllegalArgumentException ex) {
+            message = ex.getMessage();
+        }
+        Assertions.assertEquals("Distance cannot be negative.", message);
+    }
+
+    @Test
+    void getNameTest() {
+        Horse horse = new Horse("Winner", 2, 2);
+        Assertions.assertEquals("Winner", horse.getName());
+    }
+
+    @Test
+    void getSpeedTest() {
+        Horse horse = new Horse("Winner", 2, 2);
+        Assertions.assertEquals(2, horse.getSpeed());
+    }
+
+    @Test
+    void getDistanceTest() {
+        Horse horse = new Horse("Winner", 2, 3);
+        Assertions.assertEquals(3, horse.getDistance());
+    }
+
+    @Test
+    void moveTestInvokeStaticMethod() {
+        try (MockedStatic<Horse> ms = Mockito.mockStatic(Horse.class)) {
+            new Horse("horse", Mockito.anyDouble(), Mockito.anyDouble()).move();
+            ms.verify(() -> Horse.getRandomDouble(0.2, 0.9));
+        }
+    }
+
+    @Test
+    void moveTestMockMethodGetRandom() {
+        try (MockedStatic<Horse> ms = Mockito.mockStatic(Horse.class)) {
+            ms.when(() -> Horse.getRandomDouble(0.2, 0.9)).thenReturn(10.0);
+        }
     }
 }
